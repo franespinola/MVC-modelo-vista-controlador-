2b13b46a6840896e95715dd37c83b9b06b70d76b
francisco espinola <franespinola_92@hotmail.com>
2021-06-29 22:38:42 -0300

    " "

--- materiales/src/controllers/productsController.js
+++ materiales/src/controllers/productsController.js
@@ -9,22 +9,32 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
+		res.render('products',{productos:products})
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		// Do the magic
+		let idProducto=req.params.id;
+		for(let i=0;i<products.length;i++){
+			if(products[i].id==idProducto){
+				var productoEncontrado=products[i];
+			}
+		}
+		res.render('detail',{productoEnDetalle:productoEncontrado});
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		res.render('product-create-form')
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+		let idNuevo=products[products.length-1].id + 1
+		let nuevoObjeto=Object.assign({id:idNuevo},req.body);
+		products.push(nuevoObjeto);
+		fs.writeFileSync(productsFilePath,JSON.stringify(products,null,' '));
+		res.redirect('/')
 	},
 
 	// Update - Form to edit
